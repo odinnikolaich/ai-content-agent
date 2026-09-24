354f524a40f246acff7f6d328a1965fd4d59d5dd
odinnikolaich <walentino494@gmail.com>
2026-09-24 19:14:37 +0500

    Replace hardcoded demo with universal prompt-to-video pipeline

--- remotion/src/index.tsx
+++ remotion/src/index.tsx
@@ -2,15 +2,31 @@ import React from "react";
 import {Composition, registerRoot} from "remotion";
 import {RealVertical} from "./RealVertical";
 
+type Props = {
+  assets: Array<{title: string; image: string; start?: number; end?: number}>;
+  audio: string;
+  captions: Array<{start: number; end: number; text: string}>;
+  duration_seconds: number;
+  fps: number;
+  topic: string;
+  title?: string;
+};
+
 export const RemotionRoot: React.FC = () => (
   <Composition
     id="RealVertical"
     component={RealVertical}
-    durationInFrames={43 * 30}
+    durationInFrames={50 * 30}
     fps={30}
     width={1080}
     height={1920}
-    defaultProps={{assets: [], audio: "voice_ru.mp3", duration_seconds: 43, topic: "", narration: ""}}
+    defaultProps={{assets: [], audio: "voice_ru.mp3", captions: [], duration_seconds: 50, fps: 30, topic: "", title: ""}}
+    calculateMetadata={({props}) => ({
+      durationInFrames: Math.round(props.duration_seconds * props.fps),
+      fps: props.fps,
+      width: 1080,
+      height: 1920,
+    })}
   />
 );
 
